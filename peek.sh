50ad3b9d2d34d0ab431fe022739ac5bfd708a066
Maria Pau <mariapau@MacBook-Pro-de-Maria-3.local>
2025-10-24 11:31:03 +0200

    first commit

---
+++ peek.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+filename=$1
+num_lines=$2
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <file> <num_lines>"
+  exit 1
+fi
+
+
+head -n 3 $(find ../../dataset1/tables -type f -name "target_prokaryotes.txt")
+echo ...
+tail -n 3 $(find ../../dataset1/tables -type f -name "target_prokaryotes.txt")
